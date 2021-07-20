72d1991c34af9bd0f8236e055e7f9d63ee8491f2
Antonio-Lima <gutto1998@gmail.com>
2021-07-20 16:17:54 -0300

    Finalizada estilização do modal e carregando tabela, de acordo com dados da API.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 12000,
+          category: 'Desenvolvimento',
+          type: 'deposit',
+          createdAt: new Date('2021-01-31 10:00:00')
+        },
+        {
+          id: 2,
+          title: 'Parcela',
+          amount: 1000,
+          category: 'Apartamento',
+          type: 'withdraw',
+          createdAt: new Date('2021-02-15 09:00:00')
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
